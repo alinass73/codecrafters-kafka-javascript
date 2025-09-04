f97bcdd6ac95c854a9932427f5e731a336f17e2b
ali-nass <ali.slman7.98@gmail.com>
2025-09-04 20:40:30 +0300

    step1 done

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  connection.write("\0\0\0\x08\0\0\0\x07");
 });
 //
 server.listen(9092, "127.0.0.1");
